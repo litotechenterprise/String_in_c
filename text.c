974aa82b9b6fca352adaf9ee3b3fe68b0751f56f
Pablo <litotechenterprise@gmail.com>
2020-08-31 11:45:20 -0400

    inital commit

--- text.c
+++ text.c
@@ -4,42 +4,35 @@
 #include <time.h>
 
 int main(){
-    
-    // Setting up the Var's for the game
-    int randomNumber = 0;
-    int guess;
-    int MaxNumOfGuesses;
-    time_t t;
+    int p;
+    int i;
+    int primes[50] = {0};
+    int primeIndex = 2;
+    bool isPrime;
 
-    // intialize of the random number generator
-    srand((unsigned) time(&t));
+    primes[0] = 2;
+    primes[1] = 3;
 
-    // get the random number
-    randomNumber = rand() % 21;
+    for(p = 5; p <= 100; p = p + 2) {
 
-    printf("This is a guessing game\n");
-    printf("Pick a number 1-20");
+        isPrime = true;
 
-
-    for(MaxNumOfGuesses = 5; MaxNumOfGuesses > 0; --MaxNumOfGuesses) {
-        printf("\nYou have %d tr%s left.", MaxNumOfGuesses, MaxNumOfGuesses == 1 ? "y" : "ies");
-        printf("\nEnter you guess:  ");
-        scanf("%d", &guess);
-        
-        if(guess == randomNumber){
-            printf("\nCongrates you got it right it was %i", guess);
-            break;
-        } else if( guess < 0 || guess > 20 ){
-            printf("You game an invalid guess!");
-        } else if (guess > randomNumber){
-            printf("Your guess of %i is too high", guess);
-        }else {
-            printf("Your guess of %i is too low", guess);
+        for(i=1; isPrime && p / primes[i] >= primes[i]; i++){
+            if(p % primes[i] == 0) {
+                isPrime = false;
+            }
         }
 
-        printf("\n");
+        if(isPrime == true) {
+            primes[primeIndex] = p;
+            primeIndex = primeIndex + 1;
+        }
     }
-    printf("\n play again");
+
+    for (i=0;i<primeIndex; i++) {
+        printf("%i, ", primes[i]);
+    }
+
     
     return 0;
 }
